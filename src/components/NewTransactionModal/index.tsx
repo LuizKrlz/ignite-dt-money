43fab161af89b80ff2c4e9d2ab2872b175c6709b
Luiz Correa <luiz.correa@laborit.com.br>
2021-03-19 08:59:24 -0300

    finally module

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,9 @@
-import { FormEvent, useContext, useState } from "react";
+import { FormEvent, useState } from "react";
 import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
-import { TransactionsContext } from "../../TransactionsContext";
+import { useTransactions } from "../../hooks/useTransactions";
 import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 
 interface NewTranscationModalProps {
@@ -15,22 +15,34 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTranscationModalProps) {
-  const { createTransaction } = useContext(TransactionsContext);
+  const { createTransaction } = useTransactions();
 
   const [title, setTitle] = useState("");
   const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState("");
   const [type, setType] = useState("deposit");
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    createTransaction({
-      title,
-      amount,
-      type,
-      category,
-    });
+    try {
+      await createTransaction({
+        title,
+        amount,
+        type,
+        category,
+      });
+
+      //clear fields
+      setTitle("");
+      setAmount(0);
+      setCategory("");
+      setType("deposit");
+
+      onRequestClose();
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
